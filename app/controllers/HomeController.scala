4f1be88328c1968bce62eaaca8f97587660737d3
David Alvarez <desteban1117@gmail.com>
2017-05-17 20:43:05 -0500

    make it better

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -18,7 +18,7 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index() = Action {
-    Ok("hola muchachos")
+    Ok("hola muchachos 2")
   }
 
 }
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -14,7 +14,7 @@ class HomeControllerSpec extends PlaySpec with Results {
       val controller = new HomeController()
       val result: Future[Result] = controller.index().apply(FakeRequest())
       val bodyText: String = contentAsString(result)
-      bodyText mustBe "hola muchachos"
+      bodyText mustBe "hola muchachos 2"
     }
   }
 }
